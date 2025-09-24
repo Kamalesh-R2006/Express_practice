433ae99a617023467b10b529bff8190fe2c69b63
Kamalesh-R2006 <kamaleshramesh832006@gmail.com>
2025-09-24 12:06:01 +0530

    I have uploaded the sever program to print Hello ,[name]

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ app.get('/', (req, res) => {
     res.send('Hello World')
 })
 
+app.get('/user/:name', (req, res) => {
+    const name = req.params.name
+    res.send(`Hello, ${name}!`)
+})
+
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`)
 })
