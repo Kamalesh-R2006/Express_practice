f1f410b4fd45e3a3cff24c158a54622a9ffcb914
Kamalesh-R2006 <kamaleshramesh832006@gmail.com>
2025-09-24 12:13:10 +0530

    Implemented middleware that logs the current timestamp

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use((req, res, next) => {
+    console.log('Time:', new Date().toISOString())
+    next()
+})
+
+
 app.get('/', (req, res) => {
     res.send('Hello World')
 })
